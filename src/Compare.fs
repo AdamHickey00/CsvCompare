e17510be21028627652d1d9f41c641292f13393d
Adam Hickey <adam.hickey00@gmail.com>
2015-12-01 19:52:10 -0500

    Code commit

---
+++ src/Compare.fs
@@ -0,0 +1,62 @@
+﻿module Compare
+
+    open FSharp.Data
+    open System
+    open System.Linq
+
+    type inputFile = CsvProvider<"./data/templates/InputTemplate.csv">
+    type contactInput = CsvProvider<"./data/templates/ContactInput.csv">
+    type leadInput = CsvProvider<"./data/templates/LeadInput.csv">
+    type contactOutput = CsvProvider<"./data/templates/ContactOutput.csv">
+    type leadOutput = CsvProvider<"./data/templates/LeadInputOutput.csv">
+
+    let httpPrefix = "https://na28.salesforce.com/"
+
+    let contactEmailExists (email:string) (contacts:contactInput) =
+        contacts.Rows 
+        |> Seq.exists(fun row -> String.Equals(row.Email, email, StringComparison.OrdinalIgnoreCase))
+
+    let getContactOutputRow (inputRow:inputFile.Row) (contacts:contactInput) = 
+        let result = 
+            contacts.Rows 
+            |> Seq.filter(fun row -> String.Equals(row.Email, inputRow.``Email Address``, StringComparison.OrdinalIgnoreCase))        
+            |> Seq.map(fun row -> 
+                        new contactOutput.Row(
+                            httpPrefix + row.``Account Long ID``, 
+                            httpPrefix + row.``Contact Long ID``, 
+                            inputRow.``First Name``, 
+                            inputRow.``Last Name``, 
+                            inputRow.Company,
+                            inputRow.``Email Address``, 
+                            inputRow.``Work City``, 
+                            inputRow.``Work State``)
+                       )
+            |> Seq.head
+
+        result
+
+    let emailMatches (email:string) (contacts:contactInput) (leads:leadInput) =
+        let existsInContacts = 
+            contacts.Rows 
+            |> Seq.exists(fun row -> String.Equals(row.Email, email, StringComparison.OrdinalIgnoreCase))
+
+        let existsInLeads = 
+            leads.Rows 
+            |> Seq.exists(fun row -> String.Equals(row.Email, email, StringComparison.OrdinalIgnoreCase))
+
+        existsInContacts || existsInLeads
+
+    let namesMatch (firstName:string) (lastName:string) (contacts:contactInput) (leads:leadInput) =
+        let existsInContacts = 
+            contacts.Rows 
+            |> Seq.exists(fun row -> 
+                            String.Equals(row.``First Name``, firstName, StringComparison.OrdinalIgnoreCase)
+                            && String.Equals(row.``Last Name``, lastName, StringComparison.OrdinalIgnoreCase))
+
+        let existsInLeads = 
+            leads.Rows 
+            |> Seq.exists(fun row -> 
+                            String.Equals(row.``First Name``, firstName, StringComparison.OrdinalIgnoreCase)
+                            && String.Equals(row.``Last Name``, lastName, StringComparison.OrdinalIgnoreCase))
+
+        existsInContacts || existsInLeads
---
+++ src/Program.fs
@@ -0,0 +1,71 @@
+﻿open Compare
+open FSharp.Data
+open System
+open System.Linq
+
+    // take contacts first, then check leads
+
+    // 
+
+
+    // leads
+    // hyperlink + lead long id
+    // record owner
+
+    // contacts
+    // hyperlink + contact id
+    // hyperlink + account long id
+
+[<EntryPoint>]
+let main argv = 
+    let inputData = inputFile.Load("../../data/InputFile.csv")
+    let contacts = contactInput.Load("../../data/ContactExport.csv")
+    let leads = leadInput.Load("../../data/LeadExport.csv")   
+
+    Console.WriteLine(sprintf "Total input data rows = %i " (inputData.Rows.Count()))
+    Console.WriteLine(sprintf "Total contacts rows = %i " (contacts.Rows.Count()))
+    Console.WriteLine(sprintf "Total leads rows = %i " (leads.Rows.Count()))
+
+    // matched contacts by email
+    Console.WriteLine("Checking contact records for emails...")
+    let contactEmailOutput = new contactOutput()
+
+    let contactMatchedEmails = 
+        inputData.Rows
+        |> Seq.filter(fun row -> contactEmailExists row.``Email Address`` contacts)
+        |> Seq.map(fun row -> getContactOutputRow row contacts)
+        |> Seq.toArray
+
+    let contactEmailOutput = contactEmailOutput.Append contactMatchedEmails
+    contactEmailOutput.Save("../../data/output/ContactMatchedEmails.csv")
+
+
+
+
+    // matched leads by email
+
+    //Console.WriteLine("Checking lead records for emails...")
+    //let leadMatchedEmails = inputData.Filter(fun row -> emailMatches row.``Email Address`` contacts leads)
+    //leadMatchedEmails.Save("../../data/output/LeadMatchedEmails.csv")
+    //Console.WriteLine(sprintf "Found %i matched emails" (leadMatchedEmails.Rows.Count()))
+
+    //let unMatched = inputData.Filter(fun row -> not (emailMatches row.``Email Address`` contacts leads))
+    
+    
+
+
+
+    
+
+    // matched by name exactly
+    //Console.WriteLine("Checking names...")
+    //let matchedNames = unMatched.Filter(fun row -> namesMatch row.``First Name`` row.``Last Name`` contacts leads)
+    //let unMatchedNames = unMatched.Filter(fun row -> not (namesMatch row.``First Name`` row.``Last Name`` contacts leads))
+    //Console.WriteLine(sprintf "Found %i matched names" (matchedNames.Rows.Count()))
+
+    // last name plus first three letters of first name fuzzy
+
+
+    //unMatchedNames.Save("../../data/output/NeedsReview.csv")
+
+    0 

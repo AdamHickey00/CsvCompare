7e1f48a54bb0f56cd66d729f57d9d1a72fc840d6
Adam Hickey <adam.hickey00@gmail.com>
2015-12-03 16:15:32 -0500

    Condense match functions.

---
+++ src/Match.fs
@@ -0,0 +1,39 @@
+﻿module Match
+
+    open Contacts
+    open Files
+    open FSharp.Data
+    open Leads
+    open System
+    open System.Linq
+
+    let matchOutput 
+        (inputData:Runtime.CsvFile<inputFile.Row>)
+        (knownLeads:'InputFile) 
+        (getMatched:seq<inputFile.Row> -> 'InputFile -> Runtime.CsvFile<'OutputRow>) 
+        (comparison:inputFile.Row -> 'InputFile -> bool)
+        (outputFileName:string) =
+
+        let matchedOutput = getMatched inputData.Rows knownLeads
+        matchedOutput.Save(outputFileName)
+        inputData.Filter(fun row -> not (comparison row knownLeads))
+
+    let matchAll (inputData:inputFile) (contacts:contactInput) (leads:leadInput) =
+
+        // get matched contact emails
+        let unMatched = matchOutput inputData contacts getMatchedContactsByEmail contactEmailExists contactEmailOutputFile
+ 
+        // get matched lead emails
+        let unMatched = matchOutput unMatched leads getMatchedLeadsByEmail leadEmailExists leadEmailOutputFile
+
+        // get matched contact names
+        let unMatched = matchOutput unMatched contacts getMatchedContactsByName contactNameExists contactNameOutputFile
+
+        // get matched lead names
+        let unMatched = matchOutput unMatched leads getMatchedLeadsByName leadNameExists leadNameOutputFile
+
+        // get matched fuzzy contact names
+        let unMatched = matchOutput unMatched contacts getMatchedContactsByFuzzyName contactFuzzyNameExists contactNameFuzzyOutputFile
+
+        // get matched fuzzy lead names
+        matchOutput unMatched leads getMatchedLeadsByFuzzyName leadFuzzyNameExists leadNameFuzzyOutputFile
--- src/Program.fs
+++ src/Program.fs
@@ -2,19 +2,9 @@
 open Files
 open FSharp.Data
 open Leads
+open Match
 open System
 open System.Linq
-
-let matchContactOutput 
-    (inputData:Runtime.CsvFile<inputFile.Row>)
-    (contacts:contactInput) 
-    (getMatched:seq<inputFile.Row> -> contactInput -> Runtime.CsvFile<contactOutput.Row>) 
-    (comparison:inputFile.Row -> contactInput -> bool)
-    (outputFileName:string) =
-
-    let matchedOutput = getMatched inputData.Rows contacts
-    matchedOutput.Save(outputFileName)
-    inputData.Filter(fun row -> not (comparison row contacts))
 
 [<EntryPoint>]
 let main argv = 
@@ -39,29 +29,7 @@ let main argv =
         Console.WriteLine(sprintf "Total contacts rows = %i " (contacts.Rows.Count()))
         Console.WriteLine(sprintf "Total leads rows = %i " (leads.Rows.Count()))
 
-        // get matched contact emails
-        let unMatched = matchContactOutput inputData contacts getMatchedContactsByEmail contactEmailExists contactEmailOutputFile
- 
-        // get matched lead emails
-        let leadEmailOutput = getMatchedLeadsByEmail unMatched.Rows leads
-        leadEmailOutput.Save(leadEmailOutputFile)
-        let unMatched = unMatched.Filter(fun row -> not (leadEmailExists row leads))
-
-        // get matched contact names
-        let unMatched = matchContactOutput unMatched contacts getMatchedContactsByName contactNameExists contactNameOutputFile
-
-        // get matched lead names
-        let leadNameOutput = getMatchedLeadsByName unMatched.Rows leads
-        leadNameOutput.Save(leadNameOutputFile)
-        let unMatched = unMatched.Filter(fun row -> not (leadNameExists row leads))
-
-        // get matched fuzzy contact names
-        let unMatched = matchContactOutput unMatched contacts getMatchedContactsByFuzzyName contactFuzzyNameExists contactNameFuzzyOutputFile
-
-        // get matched fuzzy lead names
-        let leadFuzzyNameOutput = getMatchedLeadsByFuzzyName unMatched.Rows leads
-        leadFuzzyNameOutput.Save(leadNameFuzzyOutputFile)
-        let unMatched = unMatched.Filter(fun row -> not (leadFuzzyNameExists row leads))
+        let unMatched = matchAll inputData contacts leads
 
         // save unmatched records
         Console.WriteLine("Saving unmatched records...")
